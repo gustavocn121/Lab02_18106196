cf72a20d86c94da976705986f3504b6d68521e46
gustavocn121 <gustavo.cn121@gmail.com>
2026-04-06 16:36:37 -0300

    feat: create silver anac_voos table

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,34 +1,29 @@
-CREATE TABLE dim_data (
-    data_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    data DATE,
-    ano INTEGER,
-    trimestre INTEGER,
-    mes INTEGER
-);
+CREATE SCHEMA IF NOT EXISTS silver;
 
-CREATE TABLE dim_aeroporto (
-    aeroporto_id INT PRIMARY KEY,
-    nome TEXT,
-    cidade TEXT,
-    estado TEXT,
-    regiao TEXT
-);
-
-CREATE TABLE dim_empresa (
-    empresa_id INT PRIMARY KEY,
-    nome_empresa TEXT,
-    sigla_iata TEXT,
-    pais TEXT
-);
-
-CREATE TABLE fato_voos (
-    id SERIAL PRIMARY KEY,
-    data_id INT,
-    empresa_id INT,
-    aeroporto_origem_id INT,
-    aeroporto_destino_id INT,
-    quantidade_voos INT,
-    passageiros INT,
-    carga_kg TEXT,
-    receita NUMERIC
+CREATE TABLE silver.anac_voos (
+    id_basica               TEXT,
+    id_empresa              INTEGER,
+    nm_empresa              TEXT,
+    sg_empresa_iata         TEXT,
+    nm_pais                 TEXT,
+    id_aerodromo_origem     INTEGER,
+    nm_aerodromo_origem     TEXT,
+    nm_municipio_origem     TEXT,
+    sg_uf_origem            TEXT,
+    nm_regiao_origem        TEXT,
+    id_aerodromo_destino    INTEGER,
+    nm_aerodromo_destino    TEXT,
+    nm_municipio_destino    TEXT,
+    sg_uf_destino           TEXT,
+    nm_regiao_destino       TEXT,
+    dt_referencia           DATE,
+    nr_ano_referencia       INTEGER,
+    nr_trimestre_referencia INTEGER,
+    nr_mes_referencia       INTEGER,
+    nr_decolagem            INTEGER,
+    nr_passag_pagos         FLOAT,
+    kg_carga_paga           FLOAT,
+    nr_horas_voadas         FLOAT,
+    km_distancia            FLOAT,
+    nr_assentos_ofertados   FLOAT
 );
