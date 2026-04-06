9c7402c0f95c0a4dd27575857d0a051bd837eda7
gustavocn121 <gustavo.cn121@gmail.com>
2026-04-06 15:41:44 -0300

    feat: removed quality step

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ import os
 from src.ingestion.job import run as ingestion_run
 from src.load.job import run as load_run
 from src.processing.job import run as processing_run
-from src.quality.validate_raw import run as validate_raw_run
 from src.utils import read_config
 
 logging.basicConfig(
@@ -19,7 +18,6 @@ def main():
     ingestion_run(config)
     processing_run(config)
     load_run(config)
-    validate_raw_run(config)
 
 
 if __name__ == "__main__":
